274d7038a2a994fb177ce9475014b08bcff260d6
Nithinton <nithinton@gmail.com>
2024-03-22 23:04:19 +0530

    API schema validation using pydantic

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+
 app = FastAPI()
 
+class PostRequestDTO(BaseModel):
+    title: str
+    content: str
 
 @app.get("/")
 async def home():
     return {"message": "hello world"}
 
 @app.post("/createpost")
-async def createPost(payload: dict = Body(...)):
+async def createPost(payload: PostRequestDTO):
+    print(payload)
     return (payload)
