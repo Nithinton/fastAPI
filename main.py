3d85b9990c72a2758f5858c3e547fea93a5439f8
Nithinton <nithinton@gmail.com>
2024-03-22 23:27:29 +0530

    Setting  optional value using pydantic

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
 from datetime import datetime
+from typing import Optional
 
 app = FastAPI()
 
@@ -9,6 +10,10 @@ class PostRequestDTO(BaseModel):
     title: str
     content: str
     createdAt : datetime = datetime.now()
+    # rating : int = None 
+    # This means that if rating is not provided when creating an instance of the model, it will default to None. 
+    # However, once the instance is created, rating must be an integer and cannot be None
+    rating : Optional[int] = None
 
 @app.get("/")
 async def home():
