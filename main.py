27379976a59fde9a5827f48366cb5de94fb4a5e2
Nithinton <nithinton@gmail.com>
2024-03-22 20:34:21 +0530

    creating an hello world POST API

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 app = FastAPI()
 
 
 @app.get("/")
 async def home():
     return {"message": "hello world"}
+
+@app.post("/createpost")
+async def createPost(payload: dict = Body(...)):
+    return (payload)
