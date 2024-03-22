9ffcaaab384a83140f825c26e662d434d811d2d8
Nithinton <nithinton@gmail.com>
2024-03-22 23:10:29 +0530

    Setting default value in pydatic schema

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
+from datetime import datetime
 
 app = FastAPI()
 
 class PostRequestDTO(BaseModel):
     title: str
     content: str
+    createdAt : datetime = datetime.now()
 
 @app.get("/")
 async def home():
